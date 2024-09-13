98a71ced897e2e4565da51e894b267db18b4edfe
AnaStadelhofer <ana.stadelhofer@outlook.com>
2024-09-13 14:11:41 -0300

    reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,29 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  experimentalInteractiveRunEvents: true,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: false,
+      overwrite: true,
+      html: true,
+      json: true,
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('file:preprocessor', cucumber())
     },
     specPattern: "cypress/e2e/step_definitions/*.feature",
     viewportWidth: 1280,
     viewportHeight: 720,
-    video: true, // Ativa a gravação de vídeos
-    videoCompression: 32, // Define a compressão do vídeo (0-100), onde 0 é sem compressão
-    videosFolder: 'cypress/videos', // Pasta onde os vídeos serão salvos
+    video: true,
+    videoCompression: 32,
+    videosFolder: 'cypress/videos',
   },
 });
